61a58adb16005dcde9331fec8b5e64e9ac2229dc
Corax73 <ramaz999@inbox.ru>
2024-02-11 00:03:06 +0400

    added product model, migration, factory, seeder, scope, output and pagination

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        return view('layouts.index');
+        $products = Product::dateDescending();
+        return view('layouts.index', ['products' => $products]);
     }
 
     public function show()
